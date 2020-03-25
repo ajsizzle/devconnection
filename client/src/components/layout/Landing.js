49dfce9d3c4d6dbaf2a84454a41b519b7bbfcb0c
Alex <ajohnson2389@gmail.com>
2020-03-25 14:48:33 -0400

    React router dom setup

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export const Landing = () => {
+const Landing = () => {
     return (
         <section className="landing">
       <div className="dark-overlay">
         <div className="landing-inner">
-          <h1 className="x-large">DevConnection</h1>
+          <h1 className="x-large">The DevConnection</h1>
           <p className="lead">
             Create a developer profile/portfolio, share posts and get help from
             other developers
           </p>
           <div className="buttons">
-            <a href="register.html" className="btn btn-primary">Sign Up</a>
-            <a href="login.html" className="btn btn-light">Login</a>
+            <Link to='/register' className="btn btn-primary">Sign Up</Link>
+            <Link to='/login' className="btn btn-light">Login</Link>
           </div>
         </div>
       </div>
