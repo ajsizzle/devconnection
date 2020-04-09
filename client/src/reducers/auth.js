1fbf6fadf2e348c1c47a36cc4a11ce5ca9570eb3
Alex <ajohnson2389@gmail.com>
2020-04-09 15:23:57 -0400

    User Login and redirect route to user's dashboard created

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -2,7 +2,9 @@ import {
     REGISTER_SUCCESS,
     REGISTER_FAIL,
     USER_LOADED,
-    AUTH_ERROR
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL
 } from '../actions/types';
 
 const initialState = {
@@ -10,7 +12,7 @@ const initialState = {
     isAuthenticated: null,
     loading: true,
     user: null
-}
+};
 
 export default function(state = initialState, action) {
     const { type, payload } = action;
@@ -24,15 +26,17 @@ export default function(state = initialState, action) {
                 user: payload
             }
         case REGISTER_SUCCESS:
-        localStorage.setItem('token', payload.token);
-        return {
-            ...state,
-            ...payload,
-            isAuthenticated: true,
-            loading: false
+        case LOGIN_SUCCESS:
+            localStorage.setItem('token', payload.token);
+            return {
+                ...state,
+                ...payload,
+                isAuthenticated: true,
+                loading: false
         }
         case REGISTER_FAIL:
         case AUTH_ERROR:
+        case LOGIN_FAIL:
             localStorage.removeItem('token');
             return {
                 ...state,
@@ -43,4 +47,4 @@ export default function(state = initialState, action) {
         default:
             return state;
     }
-}
+};
