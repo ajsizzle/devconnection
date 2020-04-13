afd2e2299662c9eea61300e6aafd2ecde1b9dc7c
Alex <ajohnson2389@gmail.com>
2020-04-13 17:01:37 -0400

    set up dashboard on login

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -18,14 +18,14 @@ const initialState = {
 export default function(state = initialState, action) {
     const { type, payload } = action;
     
-    switch(type) {
+    switch (type) {
         case USER_LOADED:
-            return{
+            return {
                 ...state,
                 isAuthenticated: true,
                 loading: false,
                 user: payload
-            }
+            };
         case REGISTER_SUCCESS:
         case LOGIN_SUCCESS:
             localStorage.setItem('token', payload.token);
@@ -34,7 +34,7 @@ export default function(state = initialState, action) {
                 ...payload,
                 isAuthenticated: true,
                 loading: false
-        }
+        };
         case REGISTER_FAIL:
         case AUTH_ERROR:
         case LOGIN_FAIL:
@@ -45,7 +45,7 @@ export default function(state = initialState, action) {
                 token: null,
                 isAuthenticated: false,
                 loading: false
-            }
+            };
         default:
             return state;
     }
