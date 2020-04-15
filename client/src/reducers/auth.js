d6adfc08362e23a8f4fafed76b13a079954eb167
Alex <ajohnson2389@gmail.com>
2020-04-15 18:04:23 -0400

    List Education and Experiences in Dashboard

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -22,6 +22,7 @@ export default function(state = initialState, action) {
         case USER_LOADED:
             return {
                 ...state,
+                ...payload,
                 isAuthenticated: true,
                 loading: false,
                 user: payload
