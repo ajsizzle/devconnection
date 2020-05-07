a1da19ddbf6953c2f8c92303753b5efbf440e9ae
Alex <ajohnson2389@gmail.com>
2020-05-07 16:28:50 -0400

    comment display created

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
 import CommentForm from '../post/CommentForm';
+import CommentItem from '../post/CommentItem';
 import { getPost } from '../../actions/post';
 
 
@@ -13,13 +14,22 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
         getPost(match.params.id)
     }, [getPost]);
 
-    return loading || post === null ? <Spinner /> : <Fragment>
-    <Link to='/posts' className='btn'>
-        Back to Posts
-    </Link>
-        <PostItem post={post} showActions={false} />
-        <CommentForm postId={post._id} />
-    </Fragment>
+    return loading || post === null ? ( 
+        <Spinner /> 
+    ) : ( 
+        <Fragment>
+            <Link to='/posts' className='btn'>
+                Back to Posts
+            </Link>
+                <PostItem post={post} showActions={false} />
+                <CommentForm postId={post._id} />
+                <div className="comments">
+                {post.comments.map(comment => (
+                    <CommentItem key={comment._id} comment={comment} postId={post._id} />
+                ))}
+                </div>
+        </Fragment>
+    );
 };
 
 Post.propTypes = {
